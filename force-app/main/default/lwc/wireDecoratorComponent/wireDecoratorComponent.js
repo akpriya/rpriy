7b175bd12aba74d523875b3253e9e68185a83555
akpriya <rpriyahspd888@gmail.com>
2026-09-23 11:58:40 +0530

    chg

--- force-app/main/default/lwc/wireDecoratorComponent/wireDecoratorComponent.js
+++ force-app/main/default/lwc/wireDecoratorComponent/wireDecoratorComponent.js
@@ -7,6 +7,27 @@ const columns = [
 ];
 
 export default class WireDecoratorComponent extends LightningElement {
+    accountsfn;
+    errors;
     columns = columns;
     @wire(getAccountData) accounts;
+    @wire(getAccountData) accountsFunction({data, error}){
+        if(data){
+            let updatedAccounts = data.map((currItem) =>{
+                let updatedObject = {};
+                if(!currItem.Rating){
+                    updatedObject = {...currItem, Rating:"Warm"};
+                }else{
+                    updatedObject ={...currItem};
+                }
+            return updatedObject;
+        });
+            
+            this.accountsfn = [...updatedAccounts];
+            this.errors = null;
+        }else if(error){
+            this.errors = error;
+            this.accountsfn = null;
+        }
+    }
 }
